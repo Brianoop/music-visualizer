bf4fd70e22428519364ab2095cec13ac837a61c1
Brianoop <brianangoda99@gmail.com>
2021-11-04 01:12:55 +0300

    double canvas anaimation feature

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ container.addEventListener('click', function(){
     const dataArray = new Uint8Array(bufferLength);
 
 
-    const barWidth = canvas.width / bufferLength;
+    const barWidth = (canvas.width / 2) / bufferLength;
     let barHeight;
     let x;
 
@@ -58,7 +58,7 @@ file.addEventListener('change', function(){
     const dataArray = new Uint8Array(bufferLength);
 
 
-    const barWidth = canvas.width / bufferLength;
+    const barWidth = (canvas.width / 2) / bufferLength;
     let barHeight;
     let x;
 
@@ -75,10 +75,23 @@ file.addEventListener('change', function(){
 
 function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray){
     for (let i = 0; i < bufferLength; i++){
-        barHeight = dataArray[i];
+        barHeight = dataArray[i] * 2;
         const red = i * barHeight / 15;
         const green = i * 4;
         const blue = barHeight / 2;
+        canvasCtx.fillStyle = 'white';
+        canvasCtx.fillRect(canvas.width /2 - x, canvas.height - barHeight - 30, barWidth, 20);
+        canvasCtx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        canvasCtx.fillRect(canvas.width /2 - x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+    for (let i = 0; i < bufferLength; i++){
+        barHeight = dataArray[i] * 2;
+        const red = i * barHeight / 15;
+        const green = i * 4;
+        const blue = barHeight / 2;
+        canvasCtx.fillStyle = 'white';
+        canvasCtx.fillRect(x, canvas.height - barHeight - 30, barWidth, 20);
         canvasCtx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
         canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
         x += barWidth;
